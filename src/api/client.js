9b1bd6ee4d152f0b4a5ec86c50f4b3283115d363
Alvaro3c <alvaro.cuallado.g@gmail.com>
2026-05-17 11:41:07 +0200

    minor change, select options are now fetched from server

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://concert-predictor-server.onrender.com',
 })
 
 client.interceptors.response.use(
